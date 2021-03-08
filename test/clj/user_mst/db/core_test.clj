012f39c720d20740d382f17eea8451b724990015
Kenji Nagai <gitknagai44282@gmail.com>
2021-03-09 00:28:57 +0900

    add tests

--- test/clj/user_mst/db/core_test.clj
+++ test/clj/user_mst/db/core_test.clj
@@ -35,4 +35,53 @@
             :admin      nil
             :last_login nil
             :is_active  nil}
-           (db/get-user t-conn {:id "1"} {})))))
+           (first (db/get-user t-conn {:id "1"} {}))))
+    (is (= {:id         "1"
+            :first_name "Sam"
+            :last_name  "Smith"
+            :email      "sam.smith@example.com"
+            :pass       "pass"
+            :admin      nil
+            :last_login nil
+            :is_active  nil}
+           (first (db/get-user t-conn {:first_name "am"} {}))))
+    (is (= {:id         "1"
+            :first_name "Sam"
+            :last_name  "Smith"
+            :email      "sam.smith@example.com"
+            :pass       "pass"
+            :admin      nil
+            :last_login nil
+            :is_active  nil}
+           (first (db/get-user t-conn {:last_name "it"} {}))))
+    (is (= {:id         "1"
+            :first_name "Sam"
+            :last_name  "Smith"
+            :email      "sam.smith@example.com"
+            :pass       "pass"
+            :admin      nil
+            :last_login nil
+            :is_active  nil}
+           (first (db/get-user t-conn {:email "example"} {}))))
+    (is (= 1 (db/update-user!
+               t-conn
+               {:id         "1"
+                :first_name "Bob"
+                :last_name  "Smith"
+                :email      "bob.smith@example.com"
+                :pass       "pass2"
+                :target-id  "1"}
+               {})))
+    (is (= {:id         "1"
+            :first_name "Bob"
+            :last_name  "Smith"
+            :email      "bob.smith@example.com"
+            :pass       "pass2"
+            :admin      nil
+            :last_login nil
+            :is_active  nil}
+           (first (db/get-user t-conn {:email "bob"} {}))))
+    (is (= 1 (db/delete-user!
+               t-conn
+               {:id "1"}
+               {})))))
